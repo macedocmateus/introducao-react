59bc57fbb1101d6065ecc01fa2a19b38b0567ec7
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-08 19:06:22 -0300

    init css in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Button } from "./components/button";
 
+import "./style.css";
+
 export function App() {
     return (
-        <div>
+        <div className="container">
             <Button name="Salvar" onClick={() => alert("Salvo")}></Button>
             <Button name="Editar"></Button>
             <Button name="Excluir"></Button>
