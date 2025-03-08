ea33f88816750c4b08293af46b4fe8e600cb9662
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-08 18:29:31 -0300

    using props directly react component props function

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,4 @@
-interface Props {
-    name: string;
-    onClick?: () => void;
-}
+type Props = React.ComponentProps<"button"> & {};
 
 export function Button({ name, onClick }: Props) {
     return <button onClick={onClick}>{name}</button>;
