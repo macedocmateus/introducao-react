fd709b69c6ad3cf9ab1f97e9ab4ea054caf3f345
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-08 18:17:23 -0300

    add function at a component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/button";
 export function App() {
     return (
         <div>
-            <Button name="Salvar"></Button>
+            <Button name="Salvar" onClick={() => alert("Salvo")}></Button>
             <Button name="Editar"></Button>
             <Button name="Excluir"></Button>
         </div>
