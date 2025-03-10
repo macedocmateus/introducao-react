a359b3a23a5086fc3c605a5bf3d32b5a6bc7e07a
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-09 21:27:03 -0300

    creating a hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import "./global.css";
+
 import { Button } from "./components/button";
+import { useMessage } from "./hooks/useMessage";
 
 import styles from "./app.module.css";
 
 export function App() {
+    const { show } = useMessage({ name: "Mateus", age: 22 });
+
     return (
         <div className={styles.container}>
-            <Button name="Salvar" onClick={() => alert("Salvo")}></Button>
+            <Button
+                name="Adicionar"
+                onClick={() => show("Mensagem personalizada do meu hook")}
+            ></Button>
             <span>0</span>
-            <Button name="Excluir"></Button>
+            <Button name="Remover"></Button>
         </div>
     );
 }
